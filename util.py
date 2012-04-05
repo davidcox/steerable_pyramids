ad90fa025c733d838499b947da4cc0e0f02c3c04
James Bergstra <james.bergstra@gmail.com>
2012-04-05 14:50:56 -0400

    eyeball validation using ndimage.correlate instead of pythor3.fbcorr

--- util.py
+++ util.py
@@ -1,30 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from numpy import array, any, floor, concatenate, zeros
-from pythor3.operation import fbcorr
+try:
+    from pythor3.operation import fbcorr
+except ImportError:
+    from scipy.ndimage import correlate
+    def fbcorr(im, f, mode, stride, plugin=None, plugin_kwargs=None):
+        assert mode=='same'
+        if f.ndim == 3 and f.shape[0] == 1:
+            f = f[0, :, :]
+            output = correlate(im, f, mode='constant')[::stride, ::stride]
+            return output
+        elif f.ndim == 4:
+            if f.shape[3] == 1:
+                f = f[:, :, :, 0]
+                output = [correlate(im, fi, mode='nearest')
+                        [::stride, ::stride]
+                    for fi in f]
+                return np.asarray(output).transpose(1, 2, 0)
+            else:
+                raise NotImplementedError()
 
+
+def imshow(img, title=None):
+    if img.ndim == 2:
+        plt.imshow(img)
+    elif img.ndim == 3:
+        R = np.ceil(np.sqrt(img.shape[2]))
+        C = np.ceil(img.shape[2] / R)
+        R, C = map(int, (R, C))
+        for i in range(img.shape[2]):
+            plt.subplot(R, C, i + 1); plt.imshow(img[:, :, i])
+    if title:
+        plt.title(title)
+    plt.show()
 
 def max_pyramid_height(im_shape, filter_shape):
-    
+
     im_shape = array(im_shape)
     filter_shape = array(filter_shape)
-    
+
     if any(im_shape == 1):  # 1D image
         im_shape = (prod(array(im_shape)),)
         filter_shape = (prod(filter_shape),)
     elif any(filter_shape == 1):
         filter_shape = (filter_shape[0], filter_shape[0])
-    
+
     if any(im_shape < filter_shape):
         height = 0
     else:
-        height = 1 + max_pyramid_height(floor(im_shape/2.0), 
+        height = 1 + max_pyramid_height(floor(im_shape/2.0),
                                         filter_shape)
-    
+
     return height
-    
+
 
 def correlate_and_downsample(im, filt, stride=1):
+    #print 'c&d shapes', im.shape, filt.shape
     f = filt.copy()
-    
+
     if f.ndim is not 4 and f.ndim - im.ndim != 1:
         if im.ndim is 2:
             f.shape = (1, filt.shape[0], filt.shape[1])
@@ -41,11 +75,12 @@ def correlate_and_downsample(im, filt, stride=1):
 
     result = fbcorr(im, f, mode='same', stride=stride,
                     plugin=p, plugin_kwargs=pkw)
-                
+
     return result
 
 def upsample_and_correlate(im, filt, stride=(2,2), apron=(0,0)):
-        
+    #print 'u&c shapes', im.shape, filt.shape
+
     if im.ndim is 2:
         tmp = zeros( array(im.shape) * array(stride) + (array(apron)*2+1))
         tmp[apron[0]:-apron[0]:stride[0], apron[0]:-apron[0]:stride[0]] = im
